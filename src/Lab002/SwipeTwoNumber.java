28c4d860f0f941341b890bb8f351f88c582a3c9e
Sushil <sushildahale8@gmail.com>
2025-01-18 12:21:10 +0530

    18/01

---
+++ src/Lab002/SwipeTwoNumber.java
@@ -0,0 +1,19 @@
+package Lab002;
+
+public class SwipeTwoNumber {
+    public static void main(String[] args) {
+        int a= 30;
+        int b= 20;
+
+        a=a+b; //50
+
+        b= a-b; //50-20 == 30
+        a=b-10;
+
+        System.out.println("a ="+a);
+        System.out.println("b =" +b);
+
+
+
+    }
+}
---
+++ src/Lab002/multiplication.java
@@ -0,0 +1,10 @@
+package Lab002;
+
+public class multiplication {
+    public static void main(String[] args) {
+        char a='A';
+        char b='B';
+        System.out.println(a*b);
+    }
+}
+
