e4cae90bcfde8cdb0235a47abfd07c28ff902b9f
Sushil <sushildahale8@gmail.com>
2025-01-18 20:34:57 +0530

    18/01

--- src/Lab003/first.java
+++ src/Lab003/first.java
@@ -1,4 +1,12 @@
 package Lab003;
 
 public class first {
+    public static void main(String[] args) {
+        System.out.println("Hi, Dutta");
+       // System.out.println(10/0);
+
+        System.out.print("Hello World3");
+        System.out.print("Hello World4");
+    }
+
 }
